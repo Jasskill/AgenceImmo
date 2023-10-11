d85ca8a90adddf9ab9ec259922f18b45e649b8d0
F Alyssa <alyssafc8@gmail.com>
2023-10-11 17:25:57 +0200

    Ajout de fonctionnalité de mes boutons page Ajout.fxml

--- src/main/java/com/example/agenceimmo/AjoutController.java
+++ src/main/java/com/example/agenceimmo/AjoutController.java
@@ -1,5 +1,31 @@
 package com.example.agenceimmo;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import java.io.File;
+import java.io.IOException;
 public class AjoutController {
+    @FXML
+    private Label msgajoutphoto;
+    public void onAjoutPhotoclick() throws IOException {
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Choisir une photo");
+        File file = chooser.showOpenDialog(new Stage());
+
+        // Ajouter un type d'extension sur mon Chooser
+        chooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Image", "*.jpg", "*.jpeg", "*.png"));
+        if (file != null) {
+            msgajoutphoto.setText("Fichier sélectionné : " + file.getName());
+            msgajoutphoto.setVisible(true);
+        } else {
+            msgajoutphoto.setVisible(false);
+        }
+    }
+
+
+
 
 }
